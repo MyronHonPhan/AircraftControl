8691f26a12b320327fe3811e8870f52a251d622f
ultramyron <myron.phan@gmail.com>
2021-06-10 11:09:07 -0700

    Create cost.m

---
+++ cost.m
@@ -0,0 +1,11 @@
+function [f] = cost(s);
+global x u gamma
+u(1) = s(1);
+u(2) = s(2);
+x(2) = s(3);
+x(3) = x(2) + gamma;
+time = 0;
+[xd] = transp(time, x, u)
+f = xd(1)^2 + 100*xd(2)^2 + 10*xd(4)^2;
+
+
